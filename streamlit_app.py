b6450d1b9178697ef340879c76f41931f7fad4b0
vajr13 <fajarrahmatulsidiq@gmail.com>
2025-01-07 00:18:39 +0000

    Update kode

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-# Rotor default dan reflector
+# Rotor dan reflector
 rotor_1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 rotor_2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
 rotor_3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
@@ -15,79 +15,103 @@ def rotate(rotor, offset):
 def plugboard_swap(char, plugboard):
     return plugboard.get(char, char)
 
-# Fungsi enkripsi karakter
+# Proses enkripsi per karakter
 def encrypt_character(char, rotor1, rotor2, rotor3, reflector, plugboard):
-    char = plugboard_swap(char, plugboard)
-    char = rotor1[ord(char) - ord('A')]
-    char = rotor2[ord(char) - ord('A')]
-    char = rotor3[ord(char) - ord('A')]
-    char = reflector[ord(char) - ord('A')]
-    char = chr(rotor3.index(char) + ord('A'))
-    char = chr(rotor2.index(char) + ord('A'))
-    char = chr(rotor1.index(char) + ord('A'))
-    char = plugboard_swap(char, plugboard)
+    char = plugboard_swap(char, plugboard)  # Plugboard awal
+    char = rotor1[ord(char) - ord('A')]    # Rotor 1
+    char = rotor2[ord(char) - ord('A')]    # Rotor 2
+    char = rotor3[ord(char) - ord('A')]    # Rotor 3
+    char = reflector[ord(char) - ord('A')] # Reflector
+    char = chr(rotor3.index(char) + ord('A'))  # Rotor 3 balik
+    char = chr(rotor2.index(char) + ord('A'))  # Rotor 2 balik
+    char = chr(rotor1.index(char) + ord('A'))  # Rotor 1 balik
+    char = plugboard_swap(char, plugboard)  # Plugboard akhir
     return char
 
+# Proses enkripsi dengan visualisasi rotor
+def enigma_process_with_rotor_visualization(message, rotor1, rotor2, rotor3, rotor_pos1, rotor_pos2, rotor_pos3, plugboard):
+    processed_message = ""
+    rotor1 = rotate(rotor1, rotor_pos1 - 1)
+    rotor2 = rotate(rotor2, rotor_pos2 - 1)
+    rotor3 = rotate(rotor3, rotor_pos3 - 1)
+    
+    rotor_display = st.empty()  # Placeholder untuk menampilkan posisi rotor
+    
+    for char in message:
+        if char.isalpha():
+            processed_message += encrypt_character(char.upper(), rotor1, rotor2, rotor3, reflector, plugboard)
+            rotor1 = rotate(rotor1, 1)  # Rotor 1 bergerak maju
+            rotor_display.markdown(
+                f"""
+                **Posisi Rotor (bergerak):**
+                - Rotor 1: `{rotor1}`
+                - Rotor 2: `{rotor2}`
+                - Rotor 3: `{rotor3}`
+                """
+            )
+            time.sleep(0.3)  # Delay untuk membuat perubahan terlihat
+    return processed_message
+
 # Streamlit UI
-st.title("Enigma Machine with Dynamic Rotor Visualization")
+st.title("Enigma Machine Project - 2C")
+st.info("Simulasi mesin Enigma. Masukkan pesan untuk mengenkripsi atau mendekripsi.")
 
 # Input pesan
-message = st.text_input("Masukkan pesan (A-Z):", "")
+message = st.text_input("Masukkan pesan", "")
 
-# Pengaturan rotor awal
-st.subheader("Setel Rotor")
-rotor_input_1 = st.text_input("Rotor 1", rotor_1)
-rotor_input_2 = st.text_input("Rotor 2", rotor_2)
-rotor_input_3 = st.text_input("Rotor 3", rotor_3)
+# Posisi rotor
+st.subheader("Setel Posisi Rotor (1-26)")
+rotor_pos1 = st.selectbox("Posisi Rotor 1", list(range(1, 27)), index=0)
+rotor_pos2 = st.selectbox("Posisi Rotor 2", list(range(1, 27)), index=0)
+rotor_pos3 = st.selectbox("Posisi Rotor 3", list(range(1, 27)), index=0)
 
-# Validasi panjang rotor
-if len(rotor_input_1) != 26 or len(rotor_input_2) != 26 or len(rotor_input_3) != 26:
-    st.error("Rotor harus memiliki panjang 26 karakter.")
-    st.stop()
+# Plugboard
+st.subheader("Plugboard: Klik dua huruf untuk memasangkan")
+if "plugboard" not in st.session_state:
+    st.session_state.plugboard = {}
+if "selected_button" not in st.session_state:
+    st.session_state.selected_button = None
+if "pending_pair" not in st.session_state:
+    st.session_state.pending_pair = None
 
-# Posisi awal rotor
-rotor_pos1 = st.slider("Posisi Rotor 1 (1-26)", 1, 26, 1)
-rotor_pos2 = st.slider("Posisi Rotor 2 (1-26)", 1, 26, 1)
-rotor_pos3 = st.slider("Posisi Rotor 3 (1-26)", 1, 26, 1)
+cols = st.columns(13)
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-# Plugboard setup
-st.subheader("Plugboard")
-plugboard = {}
-plugboard_input = st.text_area("Masukkan pasangan plugboard (contoh: AB CD EF)", "")
-for pair in plugboard_input.split():
-    if len(pair) == 2:
-        plugboard[pair[0].upper()] = pair[1].upper()
-        plugboard[pair[1].upper()] = pair[0].upper()
+for i, char in enumerate(alphabet):
+    col = cols[i % 13]
+    paired_char = st.session_state.plugboard.get(char, "")
+    col.markdown(f"<div style='text-align: center; font-size: 1.5em;'>{paired_char}</div>", unsafe_allow_html=True)
+    if col.button(f"{char}", key=f"button_{char}"):
+        if st.session_state.selected_button is None:
+            st.session_state.selected_button = char
+        elif st.session_state.selected_button == char:
+            st.session_state.selected_button = None
+        else:
+            char1 = st.session_state.selected_button
+            char2 = char
+            st.session_state.pending_pair = (char1, char2)
+            st.session_state.selected_button = None
 
-# Proses enkripsi
+if st.session_state.pending_pair:
+    char1, char2 = st.session_state.pending_pair
+    if char1 in st.session_state.plugboard:
+        del st.session_state.plugboard[st.session_state.plugboard[char1]]
+        del st.session_state.plugboard[char1]
+    if char2 in st.session_state.plugboard:
+        del st.session_state.plugboard[st.session_state.plugboard[char2]]
+        del st.session_state.plugboard[char2]
+    time.sleep(0.5)
+    st.session_state.plugboard[char1] = char2
+    st.session_state.plugboard[char2] = char1
+    st.session_state.pending_pair = None
+
+if st.button("Reset Plugboard"):
+    st.session_state.plugboard.clear()
+    st.session_state.selected_button = None
+    st.success("Plugboard telah direset.")
+
+# Proses enkripsi dengan visualisasi rotor
 if message:
-    processed_message = ""
-    rotor1 = rotate(rotor_input_1, rotor_pos1 - 1)
-    rotor2 = rotate(rotor_input_2, rotor_pos2 - 1)
-    rotor3 = rotate(rotor_input_3, rotor_pos3 - 1)
-    rotor_display = []
-
-    for i, char in enumerate(message):
-        if char.isalpha():
-            # Enkripsi karakter
-            encrypted_char = encrypt_character(char.upper(), rotor1, rotor2, rotor3, reflector, plugboard)
-            processed_message += encrypted_char
-
-            # Rotasi rotor dan log pergerakan
-            rotor1 = rotate(rotor1, 1)
-            if i % 26 == 25:  # Rotor 2 bergerak setelah 26 rotasi
-                rotor2 = rotate(rotor2, 1)
-            if i % (26 * 26) == (26 * 26 - 1):  # Rotor 3 bergerak setelah 26*26 rotasi
-                rotor3 = rotate(rotor3, 1)
-
-            # Simpan keadaan rotor
-            rotor_display.append(f"Step {i + 1}: Rotor1={rotor1}, Rotor2={rotor2}, Rotor3={rotor3}")
-
-    # Tampilkan hasil
-    st.subheader("Hasil Enkripsi")
-    st.write("Pesan Terproses:", processed_message)
-
-    # Tampilkan log pergerakan rotor
-    st.subheader("Visualisasi Pergerakan Rotor")
-    for step in rotor_display:
-        st.text(step)
+    processed_message = enigma_process_with_rotor_visualization(message, rotor_1, rotor_2, rotor_3, rotor_pos1, rotor_pos2, rotor_pos3, st.session_state.plugboard)
+    st.subheader("Hasil Enkripsi/Dekripsi")
+    st.write("Pesan yang diproses:", processed_message)
