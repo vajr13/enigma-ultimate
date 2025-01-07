9412efb61e1588934f269755c46ee6167f6be837
vajr13 <fajarrahmatulsidiq@gmail.com>
2025-01-07 00:47:29 +0000

    Update kode

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,48 @@
 import streamlit as st
 import time
 
+# Rotor dan reflector yang digunakan
 rotor_1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 rotor_2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
 rotor_3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
 reflector = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 
+# Fungsi untuk merotasi rotor berdasarkan offset
 def rotate(rotor, offset):
     return rotor[offset:] + rotor[:offset]
 
+# Fungsi untuk plugboard swap
 def plugboard_swap(char, plugboard):
     return plugboard.get(char, char)
 
-def encrypt_character(char, rotor1, rotor2, rotor3, reflector, plugboard):
+# Fungsi untuk mengenkripsi setiap karakter
+def encrypt_character(char, rotor1, rotor2, rotor3, reflector, plugboard, rotor_pos1, rotor_pos2, rotor_pos3):
+    # Plugboard swap
     char = plugboard_swap(char, plugboard)
-    char = rotor1[ord(char) - ord('A')]
-    char = rotor2[ord(char) - ord('A')]
-    char = rotor3[ord(char) - ord('A')]
+    
+    # Proses Enkripsi melalui rotor
+    char = rotor1[(ord(char) - ord('A') + rotor_pos1) % 26]
+    char = rotor2[(ord(char) - ord('A') + rotor_pos2) % 26]
+    char = rotor3[(ord(char) - ord('A') + rotor_pos3) % 26]
+    
+    # Reflektor
     char = reflector[ord(char) - ord('A')]
+    
+    # Kembali melalui rotor
     char = chr(rotor3.index(char) + ord('A'))
     char = chr(rotor2.index(char) + ord('A'))
     char = chr(rotor1.index(char) + ord('A'))
+    
+    # Plugboard swap
     char = plugboard_swap(char, plugboard)
     return char
 
+# Fungsi untuk melakukan proses enkripsi
 def enigma_process(message, rotor1, rotor2, rotor3, rotor_pos1, rotor_pos2, rotor_pos3, plugboard):
     processed_message = ""
     for char in message:
         if char.isalpha():
-            processed_message += encrypt_character(char.upper(), rotor1, rotor2, rotor3, reflector, plugboard)
+            processed_message += encrypt_character(char.upper(), rotor1, rotor2, rotor3, reflector, plugboard, rotor_pos1, rotor_pos2, rotor_pos3)
             rotor_pos1 += 1
             if rotor_pos1 > 26:
                 rotor_pos1 = 1
@@ -43,23 +57,24 @@ def enigma_process(message, rotor1, rotor2, rotor3, rotor_pos1, rotor_pos2, roto
             processed_message += char
             yield processed_message, rotor_pos1, rotor_pos2, rotor_pos3
 
-st.title("Enigma Machine with Visible Rotor Movement")
-st.info("Simulasi mesin Enigma dengan pergerakan rotor terlihat langsung.")
+# Streamlit UI
+st.title("Simulasi Mesin Enigma")
+st.info("Simulasi mesin Enigma dengan pergerakan rotor yang terlihat.")
 
 # Input pesan menggunakan text area
-message = st.text_area("Masukkan pesan (paragraf dapat dimasukkan di sini):", "", height=150)
+message = st.text_area("Masukkan pesan:", "", height=150)
 
-# Tampilan rotor seperti gambar (dengan tombol +/-)
+# Setel posisi rotor menggunakan slider
 st.subheader("Setel Posisi Rotor (1-26)")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    rotor_pos1 = st.number_input("Rotor 1", min_value=1, max_value=26, value=1, step=1)
+    rotor_pos1 = st.number_input("Posisi Rotor 1", min_value=1, max_value=26, value=1, step=1)
 with col2:
-    rotor_pos2 = st.number_input("Rotor 2", min_value=1, max_value=26, value=2, step=1)
+    rotor_pos2 = st.number_input("Posisi Rotor 2", min_value=1, max_value=26, value=2, step=1)
 with col3:
-    rotor_pos3 = st.number_input("Rotor 3", min_value=1, max_value=26, value=3, step=1)
+    rotor_pos3 = st.number_input("Posisi Rotor 3", min_value=1, max_value=26, value=3, step=1)
 
 # Plugboard
 if "plugboard" not in st.session_state:
