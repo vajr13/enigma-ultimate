3ebe88f1e8776b691f87dbfe1d2f93bfa863aac1
vajr13 <fajarrahmatulsidiq@gmail.com>
2025-01-07 23:59:36 +0000

    Update kode

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,10 @@ if "rotor_pos2" not in st.session_state:
     st.session_state.rotor_pos2 = 1
 if "rotor_pos3" not in st.session_state:
     st.session_state.rotor_pos3 = 1
-if "processed_message" not in st.session_state:
-    st.session_state.processed_message = ""
+if "input_message" not in st.session_state:
+    st.session_state.input_message = ""
+if "output_message" not in st.session_state:
+    st.session_state.output_message = ""
 if "plugboard" not in st.session_state:
     st.session_state.plugboard = {}
 if "selected_button" not in st.session_state:
@@ -51,7 +53,8 @@ def process_character(char):
     encrypted_char = encrypt_character(
         char, rotor1, rotor2, rotor3, reflector, st.session_state.plugboard
     )
-    st.session_state.processed_message += encrypted_char
+    st.session_state.input_message += char
+    st.session_state.output_message += encrypted_char
 
     # Pergerakan Rotor
     st.session_state.rotor_pos1 += 1
@@ -67,18 +70,15 @@ def process_character(char):
 # Judul
 st.title("Enigma Machine with Integrated Rotor Input and Monitoring")
 
-# Setel dan Monitoring Posisi Rotor (dalam satu tempat)
+# Setel dan Monitoring Posisi Rotor
 st.subheader("Setel dan Monitoring Posisi Rotor (1-26)")
 col1, col2, col3 = st.columns(3)
 with col1:
     st.number_input("Rotor 1", min_value=1, max_value=26, value=st.session_state.rotor_pos1, step=1, key="rotor_input1")
-    st.markdown(f"**Posisi Saat Ini: {st.session_state.rotor_pos1}**")
 with col2:
     st.number_input("Rotor 2", min_value=1, max_value=26, value=st.session_state.rotor_pos2, step=1, key="rotor_input2")
-    st.markdown(f"**Posisi Saat Ini: {st.session_state.rotor_pos2}**")
 with col3:
     st.number_input("Rotor 3", min_value=1, max_value=26, value=st.session_state.rotor_pos3, step=1, key="rotor_input3")
-    st.markdown(f"**Posisi Saat Ini: {st.session_state.rotor_pos3}**")
 
 if st.button("Set Posisi Rotor"):
     st.session_state.rotor_pos1 = st.session_state.rotor_input1
@@ -130,13 +130,24 @@ for i, char in enumerate(alphabet):
     if col.button(char):
         process_character(char)
 
-# Reset Rotor dan Pesan
+# Tombol untuk Menghapus Karakter Terakhir
+if st.button("Hapus Karakter Terakhir"):
+    if st.session_state.input_message:
+        st.session_state.input_message = st.session_state.input_message[:-1]
+        st.session_state.output_message = st.session_state.output_message[:-1]
+
+# Dua Area untuk Teks Input dan Output
+st.subheader("Pesan Input dan Output")
+col1, col2 = st.columns(2)
+with col1:
+    st.text_area("Teks Input", value=st.session_state.input_message, height=200)
+with col2:
+    st.text_area("Teks Output (Terenkripsi)", value=st.session_state.output_message, height=200)
+
+# Tombol untuk Reset
 if st.button("Reset Rotor dan Pesan"):
     st.session_state.rotor_pos1 = 1
     st.session_state.rotor_pos2 = 1
     st.session_state.rotor_pos3 = 1
-    st.session_state.processed_message = ""
-
-# Tampilkan Pesan Terenkripsi
-st.subheader("Pesan Terenkripsi")
-st.write(st.session_state.processed_message)
+    st.session_state.input_message = ""
+    st.session_state.output_message = ""
