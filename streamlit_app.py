6c00c9fa91ccdc20183d22060b0c789a5223131a
vajr13 <fajarrahmatulsidiq@gmail.com>
2025-01-07 23:36:26 +0000

    Update kode

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import time
+import string
 
 rotor_1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 rotor_2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE"
@@ -24,102 +24,71 @@ def encrypt_character(char, rotor1, rotor2, rotor3, reflector, plugboard):
     char = plugboard_swap(char, plugboard)
     return char
 
-def enigma_process(message, rotor1, rotor2, rotor3, rotor_pos1, rotor_pos2, rotor_pos3, plugboard):
-    processed_message = ""
-    for char in message:
-        if char.isalpha():
-            processed_message += encrypt_character(char.upper(), rotor1, rotor2, rotor3, reflector, plugboard)
-            rotor_pos1 += 1
-            if rotor_pos1 > 26:
-                rotor_pos1 = 1
-                rotor_pos2 += 1
-                if rotor_pos2 > 26:
-                    rotor_pos2 = 1
-                    rotor_pos3 += 1
-                    if rotor_pos3 > 26:
-                        rotor_pos3 = 1
-            yield processed_message, rotor_pos1, rotor_pos2, rotor_pos3
-        else:
-            processed_message += char
-            yield processed_message, rotor_pos1, rotor_pos2, rotor_pos3
+# Inisialisasi posisi rotor di session state
+if "rotor_pos1" not in st.session_state:
+    st.session_state.rotor_pos1 = 1
+if "rotor_pos2" not in st.session_state:
+    st.session_state.rotor_pos2 = 1
+if "rotor_pos3" not in st.session_state:
+    st.session_state.rotor_pos3 = 1
+if "processed_message" not in st.session_state:
+    st.session_state.processed_message = ""
 
-st.title("Enigma Machine with Visible Rotor Movement")
-st.info("Simulasi mesin Enigma dengan pergerakan rotor terlihat langsung.")
+# Fungsi untuk memproses satu karakter
+def process_character(char):
+    rotor1 = rotate(rotor_1, st.session_state.rotor_pos1 - 1)
+    rotor2 = rotate(rotor_2, st.session_state.rotor_pos2 - 1)
+    rotor3 = rotate(rotor_3, st.session_state.rotor_pos3 - 1)
 
-# Input pesan menggunakan text area
-message = st.text_area("Masukkan pesan (paragraf dapat dimasukkan di sini):", "", height=150)
+    encrypted_char = encrypt_character(
+        char, rotor1, rotor2, rotor3, reflector, {}
+    )
+    st.session_state.processed_message += encrypted_char
 
-# Tampilan rotor seperti gambar (dengan tombol +/-)
+    # Pergerakan rotor
+    st.session_state.rotor_pos1 += 1
+    if st.session_state.rotor_pos1 > 26:
+        st.session_state.rotor_pos1 = 1
+        st.session_state.rotor_pos2 += 1
+        if st.session_state.rotor_pos2 > 26:
+            st.session_state.rotor_pos2 = 1
+            st.session_state.rotor_pos3 += 1
+            if st.session_state.rotor_pos3 > 26:
+                st.session_state.rotor_pos3 = 1
+
+st.title("Enigma Machine with Real-Time Rotor Movement")
 st.subheader("Setel Posisi Rotor (1-26)")
 
+# Tampilan rotor
 col1, col2, col3 = st.columns(3)
-
 with col1:
-    rotor_pos1 = st.number_input("Rotor 1", min_value=1, max_value=26, value=1, step=1)
+    st.markdown("### Rotor 1")
+    st.write(st.session_state.rotor_pos1)
 with col2:
-    rotor_pos2 = st.number_input("Rotor 2", min_value=1, max_value=26, value=2, step=1)
+    st.markdown("### Rotor 2")
+    st.write(st.session_state.rotor_pos2)
 with col3:
-    rotor_pos3 = st.number_input("Rotor 3", min_value=1, max_value=26, value=3, step=1)
+    st.markdown("### Rotor 3")
+    st.write(st.session_state.rotor_pos3)
 
-# Plugboard
-if "plugboard" not in st.session_state:
-    st.session_state.plugboard = {}
-if "selected_button" not in st.session_state:
-    st.session_state.selected_button = None
-if "pending_pair" not in st.session_state:
-    st.session_state.pending_pair = None
-
-st.subheader("Plugboard: Klik dua huruf untuk memasangkan")
+# Input melalui tombol
+st.subheader("Input Karakter (A-Z)")
 
 cols = st.columns(13)
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = string.ascii_uppercase
 
 for i, char in enumerate(alphabet):
     col = cols[i % 13]
+    if col.button(char):
+        process_character(char)
 
-    paired_char = st.session_state.plugboard.get(char, "")
-    col.markdown(f"<div style='text-align: center; font-size: 1.5em;'>{paired_char}</div>", unsafe_allow_html=True)
+# Reset rotor
+if st.button("Reset Rotor"):
+    st.session_state.rotor_pos1 = 1
+    st.session_state.rotor_pos2 = 1
+    st.session_state.rotor_pos3 = 1
+    st.session_state.processed_message = ""
 
-    if col.button(f"{char}", key=f"button_{char}"):
-        if st.session_state.selected_button is None:
-            st.session_state.selected_button = char
-        elif st.session_state.selected_button == char:
-            st.session_state.selected_button = None
-        else:
-            char1 = st.session_state.selected_button
-            char2 = char
-            st.session_state.pending_pair = (char1, char2)
-            st.session_state.selected_button = None
-
-if st.session_state.pending_pair:
-    char1, char2 = st.session_state.pending_pair
-    if char1 in st.session_state.plugboard:
-        del st.session_state.plugboard[st.session_state.plugboard[char1]]
-        del st.session_state.plugboard[char1]
-    if char2 in st.session_state.plugboard:
-        del st.session_state.plugboard[st.session_state.plugboard[char2]]
-        del st.session_state.plugboard[char2]
-    time.sleep(0.5)
-    st.session_state.plugboard[char1] = char2
-    st.session_state.plugboard[char2] = char1
-    st.session_state.pending_pair = None
-
-if st.button("Reset Plugboard"):
-    st.session_state.plugboard.clear()
-    st.session_state.selected_button = None
-    st.write("Plugboard telah direset.")
-
-# Proses enkripsi/dekripsi dengan pergerakan rotor terlihat
-if st.button("Proses"):
-    if message:
-        progress = st.empty()
-        rotors_display = st.empty()
-        encrypted_message = ""
-        for step, (encrypted_message, pos1, pos2, pos3) in enumerate(enigma_process(message, rotor_1, rotor_2, rotor_3, rotor_pos1, rotor_pos2, rotor_pos3, st.session_state.plugboard)):
-            rotors_display.write(f"Posisi Rotor: Rotor 1 = {pos1}, Rotor 2 = {pos2}, Rotor 3 = {pos3}")
-            progress.write(f"Proses: {encrypted_message}")
-            time.sleep(0.1)  # Delay untuk mensimulasikan pergerakan
-        st.success("Proses selesai!")
-        st.write("Pesan yang diproses:", encrypted_message)
-    else:
-        st.warning("Masukkan pesan terlebih dahulu!")
+# Menampilkan pesan hasil enkripsi
+st.subheader("Pesan Terenkripsi")
+st.write(st.session_state.processed_message)
